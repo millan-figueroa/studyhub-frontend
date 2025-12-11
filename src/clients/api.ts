2e9be23a19247efbc2da7ea345e9d3734794bd90
millan-figueroa <millan.fig@gmail.com>
2025-12-10 19:16:37 -0800

    Adds initial axios apiclient with backend base url configured and interceptor to attach jwt token from localStorage to all api requests

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -1,2 +1,10 @@
-export const apiClient = {} as any;
+import axios from "axios"; export const apiClient = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL, });
 
+// interceptor for protected routes
+// apiClient.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+//     }
+//     return config;
+// });
