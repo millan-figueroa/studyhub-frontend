41cb42aa679fe459ff72b2ea652f0b1d09c02570
millan-figueroa <millan.fig@gmail.com>
2025-12-10 19:50:38 -0800

    Updates navbar to use studyhub routes and logout logic, adds useauth hook for accessing authcontext

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,31 @@
-export default function Navbar() {
-  return <h1>Navbar.</h1>;
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
+
+export default function NavBar() {
+  // grab current user and logout function
+  const { user, logOut } = useAuth();
+
+  return (
+    <nav>
+      <NavLink to="/">home</NavLink>
+
+      {/* if user is not logged in show auth link */}
+      {!user && <NavLink to="/auth">signin / signup</NavLink>}
+
+      {/* if logged in show modules link */}
+      {user && <NavLink to="/modules">modules</NavLink>}
+
+      {/* logout button when logged in */}
+      {user && (
+        <NavLink
+          to="/"
+          onClick={() => {
+            logOut(); // clears user + token
+          }}
+        >
+          logout
+        </NavLink>
+      )}
+    </nav>
+  );
 }
